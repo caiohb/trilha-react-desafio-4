6418137b1a84beee85dbe95e0859161060fe032e
Caio Barbosa <caio.bar@gmail.com>
2024-01-01 17:59:30 -0300

    adding button validation

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -14,4 +14,13 @@ export const ButtonContainer = styled.button`
         opacity: 0.6;
         cursor:pointer;
     }
+
+    &:disabled {
+        background-color: #CCCCCC; /* Cor de fundo para desabilitado */
+        border: 1px solid #CCCCCC; /* Cor da borda para desabilitado */
+        color: #999999; /* Cor do texto para desabilitado */
+        cursor: not-allowed; /* Altera o cursor para 'not-allowed' */
+        opacity: 0.6;
+        cursor:pointer;
+    }
 `
